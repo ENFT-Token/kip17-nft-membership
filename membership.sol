0cf78e4dfa809f20db8815716f4048bc8cbb4154
gron1gh1 <gron1gh1@gmail.com>
2022-03-11 11:26:26 +0900

    add: mint 발급 주체 변경

--- membership.sol
+++ membership.sol
@@ -2,21 +2,24 @@ pragma solidity ^0.5.6;
 
 import {KIP17Full, Ownable, Counters} from "./KIP17/KIP17Full.sol";
 
-contract Membership is KIP17Full, Ownable {
+contract Membership is KIP17Full {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() public KIP17Full("Membership", "ENFT") {}
 
-    function mintNFT(string memory tokenURI)
+    function ownerByMember() public view returns (address[] memory) {
+        return owners[msg.sender];
+    }
+
+    function mintNFT(address _target, string memory tokenURI)
         public
-        onlyOwner
         returns (uint256)
     {
         _tokenIds.increment();
-
+        owners[msg.sender].push(_target);
         uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
+        _mint(_target, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
