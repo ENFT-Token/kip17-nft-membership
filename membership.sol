011aed5eebc07e2f3637747c25d95d379aed51af
gron1gh1 <gron1gh1@gmail.com>
2022-03-11 11:57:12 +0900

    add: strategy comment

--- membership.sol
+++ membership.sol
@@ -1,16 +1,25 @@
 pragma solidity ^0.5.6;
+pragma experimental ABIEncoderV2;
 
-import {KIP17Full, Ownable, Counters} from "./KIP17/KIP17Full.sol";
+import {KIP17Full, Counters} from "./KIP17/KIP17Full.sol";
 
 contract Membership is KIP17Full {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    mapping(address => address[]) private owners;
 
     constructor() public KIP17Full("Membership", "ENFT") {}
 
+    // 헬스장에서 소유하고 있는 회원 지갑 주소
     function ownerByMember() public view returns (address[] memory) {
         return owners[msg.sender];
     }
+
+    // myNFT 내가 소유한 NFT > 유저 측 사용
+    // 지갑에서 보유한 NFT 찾기
+    // balanceOf 개수 찾기 for
+    // tokenOfOwnerByIndex로 실제 인덱스 찾기
+    // tokenURI로 가져오기
 
     function mintNFT(address _target, string memory tokenURI)
         public
